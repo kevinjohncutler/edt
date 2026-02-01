0f44c723467d8598b28e073676d37907e6380896
Kevin Cutler <39454982+kevinjohncutler@users.noreply.github.com>
2026-01-31 23:09:22 -0800

    minor optim

--- src/_version.py
+++ src/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev276'
-__version_tuple__ = version_tuple = (0, 1, 'dev276')
+__version__ = version = '0.1.dev277'
+__version_tuple__ = version_tuple = (0, 1, 'dev277')
 
-__commit_id__ = commit_id = 'gd3020b78d'
+__commit_id__ = commit_id = 'g40e2976dc'
