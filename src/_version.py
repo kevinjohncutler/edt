523099c75074f4b33608940cb03929a2acfd14d7
Kevin Cutler <39454982+kevinjohncutler@users.noreply.github.com>
2026-02-02 23:18:57 -0800

    numa

--- src/_version.py
+++ src/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev279'
-__version_tuple__ = version_tuple = (0, 1, 'dev279')
+__version__ = version = '0.1.dev280'
+__version_tuple__ = version_tuple = (0, 1, 'dev280')
 
-__commit_id__ = commit_id = 'gf495073ee'
+__commit_id__ = commit_id = 'gc0b9f219c'
